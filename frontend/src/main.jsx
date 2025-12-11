d65a7b49b12e09ce4e21be74b707266f8201c763
kavyasri-gaddipati <gaddipatikavyasri@gmail.com>
2025-12-11 18:42:44 +0530

    Connected Frontend to Live Backend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
+import axios from 'axios'; // <-- Idi import cheyadam marchipoku
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// IMPORTANT: Nee Render Live Link ikkada pettam
+axios.defaults.baseURL = "https://sports-court-booking-platform-l9nn.onrender.com"; 
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
